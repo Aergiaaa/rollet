71275763f22c25e217421518f6bfe61c87c999d6
Aergiaaa <muzakren45@gmail.com>
2025-12-23 16:18:03 +0700

    fix:

--- internal/database/migrations/000003_create_people_data_table.up.sql
+++ internal/database/migrations/000003_create_people_data_table.up.sql
@@ -4,10 +4,6 @@ create table if not exists people_data (
   created_at timestamp default current_timestamp
 );
 
-alter table people
-  add column if not exists people_data_id 
-  integer references people_data(id) on delete cascade;
-
 create index idx_people_data_person_id on people_data(person_id);
 create index idx_people_data_user_id on people_data(user_id);
 create index idx_people_user_id on people(user_id);
